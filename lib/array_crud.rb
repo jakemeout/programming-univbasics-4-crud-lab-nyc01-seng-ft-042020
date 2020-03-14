4b06e45582f3a08c5aaddb8fa8392e98ba387ce4
Jacob Hyde <jacobosity@gmail.com>
2020-03-14 21:47:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -35,6 +35,5 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  replace = array.index(element)
-  array[replace]
+  array.index(element) = element
 end
